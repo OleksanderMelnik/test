f01a18997698c798463291b5982fdc8966ef2c0f
OleksanderMelnik <alekandrmelnik@gmail.com>
2023-09-10 13:39:32 +0300

    utils

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// git add . & git commit -m "Initial commit" можно замінити на git commit -am "Initial commit"
+
+import { add as addFunction, minus, division, multiplication } from './utils.js'
+import divisionFunction from './utils.js';
+
+
+addFunction(45, 5);
+minus (45, 10);
+divisionFunction (16, 2);
+multiplication (5, 5);
+
+import { mango } from "./utils.js";
+
+
+console.log(mango);
